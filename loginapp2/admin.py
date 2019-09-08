f01018a43f7c2a1ab51beb75d62392e85ef111cc
jiandongliru <976399651@qq.com>
2019-09-08 16:12:37 +0800

    loginapp2

--- loginapp2/admin.py
+++ loginapp2/admin.py
@@ -1,3 +1,21 @@
 from django.contrib import admin
+from .models import NavigationDetaiModel, \
+    SelectedModel, CarsouseiMapModel
+
 
 # Register your models here.
+class NavigationDetaiModelAdmin(admin.ModelAdmin):
+    list_display = ('img', 'img_id', 'img_name', 'goods_id')
+
+
+class CarsouseiMapModelAdmin(admin.ModelAdmin):
+    list_display = ('img', 'img_id', 'img_name', 'goods_id')
+
+
+class SelectedModelAdmin(admin.ModelAdmin):
+    list_display = ('img', 'img_id', 'img_name')
+
+
+admin.site.register(NavigationDetaiModel, NavigationDetaiModelAdmin)
+admin.site.register(CarsouseiMapModel, CarsouseiMapModelAdmin)
+admin.site.register(SelectedModel, SelectedModelAdmin)
--- loginapp2/models.py
+++ loginapp2/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
+
 # Create your models here.
+# 创建一个抽象模型类
+class BaseModel(models.Model):
+    img = models.ImageField(verbose_name="图片",
+                            upload_to="loginapp2")
+    img_id = models.CharField(max_length=20,
+                              verbose_name="图片ID")
+    img_name = models.CharField(max_length=100,
+                                verbose_name="图片名称")
+
+    class Meta:
+        abstract = True
+
+
+class NavigationDetaiModel(BaseModel):
+    goods_id = models.ForeignKey('goodsapp.models.GoodsModel',
+                                 verbose_name="商品ID")
+
+    def __str__(self):
+        return self.img_name
+
+    class Meta:
+        db_table = "app_navigationdetai"
+        verbose_name = verbose_name_plural = "导航详情表"
+
+
+class SelectedModel(BaseModel):
+    def __str__(self):
+        return self.img_name
+
+    class Meta:
+        db_table = "app_selected"
+        verbose_name = verbose_name_plural = "精选推荐表"
+
+
+class CarsouseiMapModel(BaseModel):
+    goods_id = models.ForeignKey("goodsapp.models.GoodsModel",
+                                 verbose_name="商品ID")
+
+    def __str__(self):
+        return self.img_name
+
+    class Meta:
+        db_table = "app_carsouseimap"
+        verbose_name_plural = verbose_name = "轮播图表"
