93c17d9b012003d9bac8522c69c3d98a6a3646cf
w771872496 <771872496@qq.com>
2019-09-08 15:57:51 +0800

    购物车app

--- shopcartapp/admin.py
+++ shopcartapp/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from .models import ShopCarModel, PictureModel
 # Register your models here.
+
+class ShopCarAdmin(admin.ModelAdmin):
+    list_display = ('user_id', 'good_id', 'count', 'is_selected')
+
+class PictureAdmin(admin.ModelAdmin):
+    list_display = ('id', 'picture_name', 'picture_path')
+
+
+admin.site.register(ShopCarModel, ShopCarAdmin)
+admin.site.register(PictureModel, PictureAdmin)
--- shopcartapp/models.py
+++ shopcartapp/models.py
@@ -1,3 +1,38 @@
 from django.db import models
 
 # Create your models here.
+class ShopCarModel(models.Model):
+    user_id = models.ForeignKey('indexapp.UserModel',
+                                on_delete=models.CASCADE,
+                                verbose_name="用户ID")
+
+    good_id = models.ForeignKey('goodsapp.GoodsModel',
+                                on_delete=models.CASCADE,
+                                verbose_name="商品ID")
+
+    count = models.IntegerField(verbose_name='数量',
+                                default=0)
+
+    is_selected = models.BooleanField(verbose_name='是否选中',
+                                      default=False)
+
+    def __str__(self):
+        return self.user_id + "-" + self.good_id
+
+    class Meta:
+        db_table = 'car_table'
+        verbose_name_plural = "购物车"
+
+class PictureModel(models.Model):
+    picture_name = models.CharField(max_length=30,
+                                    verbose_name='图片名称')
+
+    picture_path = models.ImageField(verbose_name='商品图片',
+                                     upload_to='picture')
+
+    def __str__(self):
+        return self.picture_name
+
+    class Meta:
+        db_table = 'picture_table'
+        verbose_name_plural = "图片表"
