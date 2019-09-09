19cc869791ed39881e91c3e8271ed345108aa043
jiandongliru <976399651@qq.com>
2019-09-09 21:00:27 +0800

    mm

---
+++ loginapp2/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+from .views import detaios, change, loginout, img_url
+app_name = "loginapp2"
+
+urlpatterns = [
+    path('/detaios', detaios, name="detaios"),
+    path('/change', change, name="change"),
+    path('/loginout', loginout, name="loginout"),
+    path('/imgurl', img_url, name="img_url"),
+
+]
--- loginapp2/views.py
+++ loginapp2/views.py
@@ -1,3 +1,32 @@
+from django.http import JsonResponse
 from django.shortcuts import render
 
 # Create your views here.
+
+
+def detaios(request):
+    token = request.COOKIES.get("token", None)
+    user = request.POST.get("token", None)
+    if not token:
+        return JsonResponse({
+            'code': 300,
+            'msg': '用户未登录,请重新登陆'
+        })
+    else:
+        return JsonResponse({
+            'code': 200,
+            'msg': '获取成功',
+            'name': user.name,
+            'gender': user.sex,
+            'level': user.level,
+        })
+
+
+def change(request):
+    user_name = request.POST.get('user_name', None)
+def loginout( request):
+    pass
+def upload_avator(request):
+    pass
+def img_url(request):
+    pass
