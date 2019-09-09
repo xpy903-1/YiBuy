6a521f1d4011cce67172deda8e0e1e06d5bb455e
jiandongliru <976399651@qq.com>
2019-09-09 17:16:07 +0800

    5

--- loginapp2/admin.py
+++ loginapp2/admin.py
@@ -5,23 +5,25 @@ from .models import NavigationDetaiModel, \
 
 # Register your models here.
 class NavigationDetaiModelAdmin(admin.ModelAdmin):
-    list_display = ('id', 'img_id', 'goods_id', 'img_name', 'img1', 'img_height',
-                    'img_width')
-    fields = ('img_id', 'goods_id', 'img_name', 'img1', 'img_height', 'img_width')
+    list_display = (
+    'id', 'img_id', 'goods_id', 'img_name', 'img1', 'img_height',
+    'img_width')
+    fields = (
+    'img_id', 'goods_id', 'img_name', 'img1', 'img_height', 'img_width')
 
 
 class CarsouseiMapModelAdmin(admin.ModelAdmin):
-    list_display = ('id', 'img_id', 'goods_id', 'img_name', 'img1', 'img_height',
-                    'img_width')
+    list_display = (
+    'id', 'img_id', 'goods_id', 'img_name', 'img1', 'img_height',
+    'img_width')
     fields = ('img_id', 'goods_id', 'img_name', 'img1', 'img_height',
               'img_width')
-
 
 
 class SelectedModelAdmin(admin.ModelAdmin):
     list_display = ('id', 'img_id', 'img_name', 'img1', 'img_height',
                     'img_width')
-    fields = ('img_id',  'img_name', 'img1', 'img_height', 'img_width')
+    fields = ('img_id', 'img_name', 'img1', 'img_height', 'img_width')
 
 
 admin.site.register(NavigationDetaiModel, NavigationDetaiModelAdmin)
--- loginapp2/models.py
+++ loginapp2/models.py
@@ -2,7 +2,6 @@ import uuid
 
 from django.db import models
 
-
 # Create your models here.
 # 创建一个抽象模型类
 from goodsapp.models import GoodsModel
@@ -16,22 +15,23 @@ class BaseModel(models.Model):
     img_name = models.CharField(max_length=50,
                                 verbose_name="图片名")
     img1 = models.ImageField(verbose_name="图片",
-                            upload_to='loginapp2',
-                            width_field='img_width',
-                            height_field='img_height'
-                            )
+                             upload_to='loginapp2',
+                             width_field='img_width',
+                             height_field='img_height'
+                             )
     # 图片宽度
     img_width = models.IntegerField(verbose_name='商品图片宽度',
-                                        null=True)
+                                    null=True)
 
     # 图片高度
     img_height = models.IntegerField(verbose_name='商品图片高度',
-                                         null=True)
+                                     null=True)
 
     def save(self, force_insert=False, force_update=False, using=None,
              update_fields=None):
         if self.id:
             self.id = uuid.uuid4().hex
+        super().save()
 
     class Meta:
         abstract = True
@@ -43,7 +43,7 @@ class NavigationDetaiModel(BaseModel):
                                  on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.img_id
+        return self.img_name
 
     class Meta:
         db_table = "app_navigationdetai"
@@ -53,7 +53,7 @@ class NavigationDetaiModel(BaseModel):
 class SelectedModel(BaseModel):
 
     def __str__(self):
-        return self.img_id
+        return self.img_name
 
     class Meta:
         db_table = "app_selected"
@@ -61,13 +61,12 @@ class SelectedModel(BaseModel):
 
 
 class CarsouseiMapModel(BaseModel):
-
     goods_id = models.ForeignKey(GoodsModel,
                                  verbose_name="商品ID",
                                  on_delete=models.CASCADE)
 
     def __str__(self):
-        return self.img_id
+        return self.img_name
 
     class Meta:
         db_table = "app_carsouseimap"
