e9152acc56ca1f2572cf8a2b8c2c0185f4c7327b
17629124263 <1152444399@qq.com>
2019-09-10 21:40:36 +0800

    'user'

--- indexapp/urls.py
+++ indexapp/urls.py
@@ -1,7 +1,11 @@
 from django.urls import path
-
+from indexapp.indexApi import index, eats, member_show, page_welfare, home_page
 app_name = 'indexapp'
 
 urlpatterns = [
-
+    path('nav/<category_id>', index, name='nav'),
+    path('eat/', eats, name='eat'),
+    path('show/', member_show, name='show'),
+    path('welfare/', page_welfare, name='welfare'),
+    path('page/', home_page, name='page'),
 ]
