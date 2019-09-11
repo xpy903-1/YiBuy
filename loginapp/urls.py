5eb65c7c7a14341347fb9b9c2353af33341f6282
罗鹏 <447657536@qq.com>
2019-09-11 16:15:50 +0800

    '环境'

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path
-from .views import add_city, msg_code, check_phone, login_pwd, msg_login, forgot
+from .views import msg_code, check_phone, login_pwd, msg_login, forgot
 
 app_name = 'loginapp'
 
 urlpatterns = [
-     path('add', add_city),
+     # path('add', add_city),
      path('msg_', msg_code),
      path('check', check_phone),
      path('login', login_pwd),
