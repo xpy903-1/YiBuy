7fbbd920435c185e3841547432309e623610c516
zhangshaoping123 <825886734@qq.com>
2019-09-11 21:42:23 +0800

    asc

--- addressapp/views.py
+++ addressapp/views.py
@@ -1,3 +1,5 @@
+import json
+
 from django.http import JsonResponse, HttpResponse
 from django.shortcuts import render
 
@@ -11,7 +13,7 @@ from signal import login_yz
 
 @csrf_exempt  # 跨域请求
 def address_query(request):
-    # if not login_yz.send(sender='delete_addr',request=request)[0][1]:
+    # if not login_yz.send(sender='delete_addr', request=request)[0][1]:
     #     return HttpResponse('请先登录')
     if request.method == 'POST':
         name = request.POST.get('name', None)
@@ -25,43 +27,43 @@ def address_query(request):
             if name:
                 user = UserModel.objects.filter(name=name).first()
                 address = user.address.all()
-                data = []
+                query_data = []
                 for i in address:
                     d = {
                         'ads': i.ads,
                         'name': i.name,
                         'phone': i.phone
                     }
-                    data.append(d)
+                    query_data.append(d)
                 return JsonResponse({
                     'code': 200,
                     'msg': 'ok',
-                    'data': data
+                    'data': query_data
                 })
             else:
                 user = UserModel.objects.filter(phone=phone).first()
                 address = user.address.all()
-                data = []
+                query_data = []
                 for i in address:
                     d = {
                         'ads': i.ads,
                         'name': i.name,
                         'phone': i.phone
                     }
-                    data.append(d)
+                    query_data.append(d)
                 return JsonResponse({
                     'code': 200,
                     'msg': 'ok',
-                    'data': data
+                    'data': query_data
                 })
     else:
-        return render(request, 'adsquery.html')
+        return render(request, 'address/ads_query.html')
 
 
 @csrf_exempt
 def address_add(request):
-    if not login_yz.send(sender='delete_addr',request=request)[0][1]:
-        return HttpResponse('请先登录')
+    # if not login_yz.send(sender='delete_addr', request=request)[0][1]:
+    #     return HttpResponse('请先登录')
     if request.method == 'POST':
         user_id = UserModel.objects.get(pk=request.POST.get('user_id', None))
         name = request.POST.get('name', None)
@@ -89,12 +91,12 @@ def address_add(request):
             }
         })
     else:
-        return render(request, 'adsadd.html')
+        return render(request, 'address/ads_add.html')
 
 
 @csrf_exempt
 def address_edit(request):
-    # if not login_yz.send(sender='delete_addr',request=request)[0][1]:
+    # if not login_yz.send(sender='delete_addr', request=request)[0][1]:
     #     return HttpResponse('请先登录')
     if request.method == 'POST':
         id = request.POST.get('id', None)
@@ -106,27 +108,27 @@ def address_edit(request):
                 'code': 400,
                 'msg': '请求的参数不完整'
             })
-        data = AddressModel.objects.get(pk=id)
-        data.name = name
-        data.phone = phone
-        data.ads = ads
-        data.save()
+        edit_data = AddressModel.objects.get(pk=id)
+        edit_data.name = name
+        edit_data.phone = phone
+        edit_data.ads = ads
+        edit_data.save()
         return JsonResponse({
-            'code':200,
+            'code': 200,
             'msg': 'ok',
             'data': {
-                'name': data.name,
-                'phone': data.phone,
-                'ads': data.ads
+                'name': edit_data.name,
+                'phone': edit_data.phone,
+                'ads': edit_data.ads
             }
         })
     else:
-        return render(request, 'adsedit.html')
+        return render(request, 'address/ads_edit.html')
 
 
 @csrf_exempt
 def address_delete(request):
-    # if not login_yz.send(sender='delete_addr',request=request)[0][1]:
+    # if not login_yz.send(sender='delete_addr', request=request)[0][1]:
     #     return HttpResponse('请先登录')
     if request.method == 'POST':
         id = request.POST.get('id', None)
@@ -144,4 +146,4 @@ def address_delete(request):
             })
 
     else:
-        return render(request, 'adsdelete.html')
+        return render(request, 'address/ads_delete.html')
