bbf3a4fa6d13db78513d1635d6cb498285406578
lirunhao <260440910@qq.com>
2019-09-11 20:26:02 +0800

    ''

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -1,16 +1,12 @@
 from django.urls import path
 
-from .views import msg_code, check_phone, login_pwd, msg_login, forgot
+from .views import check_phone, login_pwd
 
 
 app_name = 'loginapp'
 
 urlpatterns = [
-     # path('add', add_city),
-     path('msgcode/', msg_code, name='msgcode'),
      path('check/', check_phone, name='check'),
-     path('login/', login_pwd, name='login'),
-     path('msglogin/', msg_login, name='msglogin'),
-     path('forgot/', forgot, name='forgot')
+     path('login/', login_pwd, name='login')
 ]
 
