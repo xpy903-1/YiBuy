e7c59e6dd186e6a7ce90f79a06afd4e83f984cf3
lirunhao <260440910@qq.com>
2019-09-11 11:13:28 +0800

    ''

--- loginapp/urls.py
+++ loginapp/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
+from .views import add_city, msg_code, check_phone, login_pwd, msg_login, forgot
 
 app_name = 'loginapp'
 
 urlpatterns = [
-
+     path('add', add_city),
+     path('msg_', msg_code),
+     path('check', check_phone),
+     path('login', login_pwd),
+     path('msg', msg_login),
+     path('forgot', forgot)
 ]
 
--- loginapp/views.py
+++ loginapp/views.py
@@ -1,40 +1,65 @@
 from django.core.cache import cache
 from django.http import HttpResponse, JsonResponse
 from django.shortcuts import render
+import json
 
+from indexapp.models import UserModel
+from .models import CitysModel
 # Create your views here.
 from django.views.decorators.csrf import csrf_exempt
 
 
-@csrf_exempt
+# 短信返回
 def msg_code(request):
-    u_phone = request.POST.get('u_phone')
-
-
-def check_phone(request):
     phone = request.POST.get('phone')
-    if phone in cache:
+    u_phone = UserModel.objects.filter(phone=phone)
+    print(u_phone)
+
+    if u_phone:
+        # 手机号保存在session
+        request.session['phone'] = u_phone
+
         return JsonResponse({
-            'code': 205,
-            'msg': '手机号已存在'
+            'code': 200,
+            'msg': '短信验证码发送成功!'
         })
     else:
+        return JsonResponse({
+            'code': 303,
+            'msg': '请输入正确的手机号码'
+        })
+
+
+# 检车手机号
+def check_phone(request):
+    phone = request.GET.get('phone')
+    user = UserModel.objects.filter(phone=phone)
+    if not user:
         return JsonResponse({
             'code': 200,
             'msg': '手机号不存在'
         })
-
-
-def login_pwd(request):
-    u_phone = request.POST.get('u_phone')
-    auth_string = request.POST.get('auth_string')
-
-    if any((cache.has_key(u_phone),
-            cache.get(u_phone) == auth_string)):
+    else:
         return JsonResponse({
-            "code": "303",
-            "msg": "用户口令不正确"
+            'code': 205,
+            'msg': '手机号已存在'
         })
+
+
+# 密码登录
+def login_pwd(request):
+    phone = request.POST.get('u_phone', None)
+    pwd = request.POST.get('auth_string', None)
+
+    phone = UserModel.objects.filter(phone=phone)
+
+    if phone:
+        phone = phone.first()
+        if pwd != phone.pwd:
+            return JsonResponse({
+                "code": "303",
+                "msg": "用户口令不正确"
+            })
     else:
         return JsonResponse({
             'code': 304,
@@ -42,9 +67,65 @@ def login_pwd(request):
         })
 
 
+# 验证码登录
 def msg_login(request):
-    pass
+    phone = request.POST.get('u_phone', None)
+    code = request.POST.get('msg_code', None)
+
+    request.session['phone'] = phone
+    request.session['code'] = code
+
+    cache.set(phone, code)
+
+    return JsonResponse({
+        "code": 200,
+        "token": "b8735a654d1442cf9025bf2b14e2a309",
+        "user_data": {
+            "balance": '',
+            "gender": '',
+            "id": 1,
+            "idcard": '',
+            "img": '',
+            "is_active": 1,
+            "is_delete": 0,
+            "nickname": "YG18991708565",
+            "u_auth_string": "123456",
+            "u_level": '',
+            "u_phone": "18991708565"
+        }
+    })
 
 
+# 忘记密码
 def forgot(request):
-    return render(request, '', locals())
+    return JsonResponse({
+        "code": 200,
+        "token": "8a72951e4cee4715a5667ead5566af57",
+        "user_data": {
+            "balance": '',
+            "gender": '',
+            "id": 1,
+            "idcard": '',
+            "img": '',
+            "is_active": 1,
+            "is_delete": 0,
+            "nickname": "YG18991708565",
+            "u_auth_string": "123456",
+            "u_level": '',
+            "u_phone": "18991708565"
+        }
+    })
+
+
+def add_city(request):
+    with open('city.json', 'r', encoding='utf-8') as f:
+        city_dict = json.load(f)
+        for i in city_dict['Data']['CityList']:
+            for r in i['CityList']:
+                CitysModel.objects.create(
+                    name=r['AreaName'],
+                    start_str=r['FirstLetter'],
+                    is_popular=True
+                )
+
+    return HttpResponse('ok')
